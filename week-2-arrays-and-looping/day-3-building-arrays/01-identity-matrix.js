c2714622da10fede548612a66a5494be3f919bce
KFamurewa <kayla.famurewa@gmail.com>
2022-03-23 11:29:05 -0400

    wip-day 3 problem 2

---
+++ week-2-arrays-and-looping/day-3-building-arrays/01-identity-matrix.js
@@ -0,0 +1,49 @@
+// Description
+// Create a square 2D array of size size x size. Fill the array with 0.
+// Then draw a line of 1s down the main diagonal.
+// This is called an identity matrix.
+
+function solution(size) {
+    let row = [];
+    let twoD = [];
+    
+    while(row.length < size){
+        row.push(0);
+    }
+    
+    for(let i = 0; i < size; i++){
+        let currentRow = [...row];
+        currentRow.splice(i, 1, 1);
+        twoD.push(currentRow);
+    }
+    return twoD;
+}
+
+
+// test cases
+
+// Test 1
+// Input:
+// size: 4
+
+// Expected Output:
+// [[1,0,0,0], 
+//  [0,1,0,0], 
+//  [0,0,1,0], 
+//  [0,0,0,1]]
+
+// Test 2
+// The test case is too large and is shown truncated
+// Input:
+// size: 13
+
+// Expected Output:
+// [[1,0,0,0,0,0,0,0,0,0,0,0,0], 
+//  [0,1,0,0,0,0,0,0,0,0,0,0,0], 
+//  [0,0,1,0,0,0,0,0,0,0,0,0,0], 
+//  [0,0,0,1,0,0,0,0,0,0,0,0,0], 
+//  [0,0,0,0,1,0,0,0,0,0,0,0,0], 
+//  [0,0,0,0,0,1,0,0,0,0,0,0,0], 
+//  [0,0,0,0,0,0,1,0,0,0,0,0,0], 
+//  [0,0,0,0,0,0,0,1,0]]
+
