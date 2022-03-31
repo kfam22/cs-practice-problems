4997a7f8a0eac4e8510d19a0ea903755773460b8
KFamurewa <kayla.famurewa@gmail.com>
2022-03-30 23:54:14 -0400

    wip

---
+++ week-3-intermediate-data-structures.js/day-2-caching/01-caching-time-consuming-function.js
@@ -0,0 +1,5 @@
+// Description
+// Compute the value f(n) for a number of input integers, nums, where f() is an expensive, time-consuming function.
+// Return the results of f(n) in an array.
+// Hint: cache the results of previous calls to keep them from timing out.
+// Add this function f() before your code for solution():
---
+++ week-3-intermediate-data-structures.js/day-2-caching/02-histogram.js
@@ -0,0 +1,8 @@
+// Description
+
+// Produce a histogram of word frequency in a string.
+// The function should return an array of strings, where each string takes the form
+// word: ####
+// word is the word in question, followed by a colon and a space, followed by a number of hash marks that corresponds to the number of times that word appears in the input string
+// The result array should be sorted alphabetically. This can be done by calling .sort() of the array before returning it.
+// If the input string is empty, return an empty array.
