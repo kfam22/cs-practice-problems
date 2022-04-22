336d7643c278f4c722de8105fd8dc05feb26a72e
KFamurewa <kayla.famurewa@gmail.com>
2022-04-22 16:41:19 -0400

    fixed mistake

--- leetcode/arrays-and-linked-lists/0189-rotate-array.js
+++ leetcode/arrays-and-linked-lists/0189-rotate-array.js
@@ -1,7 +1,7 @@
 // 189. Rotate Array
 // https://leetcode.com/problems/rotate-array/
 
-Given an array, rotate the array to the right by k steps, where k is non-negative.
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
 
 
 
